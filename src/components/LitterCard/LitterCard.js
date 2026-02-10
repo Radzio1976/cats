4a9faf1157cf8a4c4f55d2bf51d653bc573ce410
Radoslaw Tomaszewski <radoslaw.tomaszewski@sobre.pl>
2026-02-10 13:45:56 +0100

    Added GatsbyImage to LitterCard component

--- src/components/LitterCard/LitterCard.js
+++ src/components/LitterCard/LitterCard.js
@@ -1,16 +1,15 @@
 import * as React from "react"
 import { Link } from "gatsby"
 import * as styles from "./LitterCard.module.css"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
 
 const LitterCard = ({ litter, urlBase, className }) => {
+  const image = getImage(litter.images?.[0]?.localFile)
   return (
     <li className={`${styles.litterCard} ${className}`}>
       <Link to={`/mioty/${litter.slug}`}>
         <div className="image-wrapper">
-          <img
-            src={litter.images?.[0]?.url || "/placeholder.jpg"}
-            alt={litter.name}
-          />
+          {image && <GatsbyImage image={image} alt={litter.name} />}
         </div>
         <h3 className={styles.litterName}>{litter.name}</h3>
       </Link>

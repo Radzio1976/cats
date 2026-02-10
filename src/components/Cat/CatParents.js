4e42074508d36b51730b50bc6a20a4cf6d838507
Radoslaw Tomaszewski <radoslaw.tomaszewski@sobre.pl>
2026-02-10 13:54:36 +0100

    Added GatsbyImage to CatParents component

--- src/components/Cat/CatParents.js
+++ src/components/Cat/CatParents.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { Link } from "gatsby"
 import * as styles from "./Cat.module.css"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
 
 const CatParents = ({ parents }) => {
   const father = parents.find(p => p.sex === "male")
@@ -8,16 +9,21 @@ const CatParents = ({ parents }) => {
 
   return (
     <div className={styles.parents}>
-      {[father, mother].map(parent => (
-        <Link to={`/dojrzale-koty/${parent?.slug}`}>
-          <div key={parent?.id} className={styles.parentCard}>
-            <img src={parent?.images[0].url} alt={parent?.images[0].fileName} />
-            <h2>
-              {parent?.sex === "male" ? "Ojciec" : "Matka"}: {parent?.name}
-            </h2>
-          </div>
-        </Link>
-      ))}
+      {[father, mother].map(parent => {
+        const image = getImage(parent?.images?.[0]?.localFile)
+        return (
+          <Link to={`/dojrzale-koty/${parent?.slug}`}>
+            <div key={parent?.id} className={styles.parentCard}>
+              {image && (
+                <GatsbyImage image={image} alt={parent?.images[0].fileName} />
+              )}
+              <h2>
+                {parent?.sex === "male" ? "Ojciec" : "Matka"}: {parent?.name}
+              </h2>
+            </div>
+          </Link>
+        )
+      })}
     </div>
   )
 }
--- src/components/OldCats/OldCats.js
+++ src/components/OldCats/OldCats.js
@@ -43,39 +43,3 @@ const OldCats = ({ oldCats }) => {
 }
 
 export default OldCats
-
-// <ul
-//   style={{
-//     padding: "0 15px",
-//   }}
-// >
-//   {oldCats.map(cat => (
-//     <Link to={`/dojrzale-koty/${cat.slug}`} key={cat.id}>
-//       <li
-//         className="old-cat-card"
-//         style={{
-//           display: "flex",
-//           flexDirection: "column",
-//           textAlign: "center",
-//           border: "1px solid black",
-//           borderRadius: "12px",
-//         }}
-//       >
-//         <div className="old-cat-image-wrapper">
-//           <img
-//             src={cat.images?.[0]?.url || "/placeholder.jpg"}
-//             alt={cat.name}
-//             style={{
-//               width: "200px",
-//               height: "200px",
-//               objectFit: "cover",
-//               objectPosition: "center",
-//               borderRadius: "12px",
-//             }}
-//           ></img>
-//         </div>
-//         <h1>{cat.name}</h1>
-//       </li>
-//     </Link>
-//   ))}
-// </ul>

4077d998346201e4d3bc9bb1ed50e67cd4c9cf9f
Radoslaw Tomaszewski <radoslaw.tomaszewski@sobre.pl>
2026-02-10 13:41:41 +0100

    Added GatsbyImage to Litter component

--- src/components/Litter/LitterChildren.js
+++ src/components/Litter/LitterChildren.js
@@ -1,26 +1,32 @@
 import React from "react"
 import { Link } from "gatsby"
 import * as styles from "./Litter.module.css"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
 
 const LitterChildren = ({ children, litterSlug }) => {
   return (
     <div className={styles.children}>
-      {children.map(child => (
-        <Link
-          to={`/mioty/${litterSlug}/${child.slug}`}
-          className={styles.link}
-          key={child.id}
-        >
-          <div className={styles.childCard}>
-            <img src={child.images[0].url} alt={child.images[0].fileName} />
-            <div>
-              <h2>{child.name}</h2>
-              <p>{child.desc.markdown}</p>
-              Więcej
+      {children.map(child => {
+        const image = getImage(child.images?.[0]?.localFile)
+        return (
+          <Link
+            to={`/mioty/${litterSlug}/${child.slug}`}
+            className={styles.link}
+            key={child.id}
+          >
+            <div className={styles.childCard}>
+              {image && (
+                <GatsbyImage image={image} alt={child.images[0].fileName} />
+              )}
+              <div>
+                <h2>{child.name}</h2>
+                <p>{child.desc.markdown}</p>
+                Więcej
+              </div>
             </div>
-          </div>
-        </Link>
-      ))}
+          </Link>
+        )
+      })}
     </div>
   )
 }
--- src/components/Litter/LitterParents.js
+++ src/components/Litter/LitterParents.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { Link } from "gatsby"
 import * as styles from "./Litter.module.css"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
 
 const LitterParents = ({ parents }) => {
   const father = parents.find(p => p.sex === "male")
@@ -8,16 +9,21 @@ const LitterParents = ({ parents }) => {
 
   return (
     <div className={styles.parents}>
-      {[father, mother].map(parent => (
-        <Link to={`/dojrzale-koty/${parent?.slug}`} key={parent?.id}>
-          <div className={styles.parentCard}>
-            <img src={parent?.images[0].url} alt={parent?.images[0].fileName} />
-            <h2>
-              {parent?.sex === "male" ? "Ojciec" : "Matka"}: {parent?.name}
-            </h2>
-          </div>
-        </Link>
-      ))}
+      {[father, mother].map(parent => {
+        const image = getImage(parent?.images?.[0]?.localFile)
+        return (
+          <Link to={`/dojrzale-koty/${parent?.slug}`} key={parent?.id}>
+            <div className={styles.parentCard}>
+              {image && (
+                <GatsbyImage image={image} alt={parent?.images[0].fileName} />
+              )}
+              <h2>
+                {parent?.sex === "male" ? "Ojciec" : "Matka"}: {parent?.name}
+              </h2>
+            </div>
+          </Link>
+        )
+      })}
     </div>
   )
 }

20408826656f4fe9fb559f70ed71147d34236470
Radoslaw Tomaszewski <radoslaw.tomaszewski@sobre.pl>
2026-02-16 15:05:12 +0100

    Finished css styling of LitterCard

--- src/components/LitterCard/LitterCard.js
+++ src/components/LitterCard/LitterCard.js
@@ -11,48 +11,48 @@ const LitterCard = ({ litter, urlBase, className }) => {
   return (
     <li className={`${styles.litterCard} ${className}`}>
       <Link to={`/mioty/${litter.slug}`}>
-        <div className="-m-1 flex flex-row ">
+        <div className="-m-1 flex flex-row h-[300px]">
           <div className="flex w-full flex-wrap">
-            <div className="w-1/2 p-1 ">
+            <div className="w-1/2 p-1 h-[45%] object-contain">
               <GatsbyImage
-                className="block h-full w-full rounded-lg 2xl:rounded-2xl object-cover object-center"
+                className="block h-full w-full rounded-lg 2xl:rounded-2xl"
                 image={getImage(litter.images?.[0]?.localFile)}
                 alt={litter.name}
               />
             </div>
-            <div className="w-1/2 p-1 ">
+            <div className="w-1/2 p-1 h-[45%]">
               <GatsbyImage
-                className="block h-full w-full rounded-lg 2xl:rounded-2xl object-cover object-center"
+                className="block h-full w-full rounded-lg 2xl:rounded-2xl"
                 image={getImage(litter.images?.[1]?.localFile)}
                 alt={litter.name}
               />
             </div>
-            <div className="w-full p-1">
+            <div className="w-full p-1 h-[55%]">
               <GatsbyImage
-                className="block h-full w-full rounded-lg 2xl:rounded-2xl object-cover object-center max-h-none lg:max-h-[1000px]"
+                className="block h-full w-full rounded-lg 2xl:rounded-2xl max-h-none lg:max-h-[1000px]"
                 image={getImage(litter.images?.[2]?.localFile)}
                 alt={litter.name}
               />
             </div>
           </div>
           <div className="flex w-full flex-wrap">
-            <div className="w-full p-1 ">
+            <div className="w-full p-1 h-[55%]">
               <GatsbyImage
-                className="block h-full w-full rounded-lg 2xl:rounded-2xl object-cover object-center"
+                className="block h-full w-full rounded-lg 2xl:rounded-2xl"
                 image={getImage(litter.images?.[3]?.localFile)}
                 alt={litter.name}
               />
             </div>
-            <div className="w-1/2 p-1 ">
+            <div className="w-1/2 p-1 h-[45%]">
               <GatsbyImage
-                className="block h-full w-full rounded-lg 2xl:rounded-2xl object-cover object-center"
+                className="block h-full w-full rounded-lg 2xl:rounded-2xl"
                 image={getImage(litter.images?.[4]?.localFile)}
                 alt={litter.name}
               />
             </div>
-            <div className="w-1/2 p-1 ">
+            <div className="w-1/2 p-1 h-[45%]">
               <GatsbyImage
-                className="block h-full w-full rounded-lg 2xl:rounded-2xl object-cover object-center"
+                className="block h-full w-full rounded-lg 2xl:rounded-2xl"
                 image={getImage(litter.images?.[5]?.localFile)}
                 alt={litter.name}
               />

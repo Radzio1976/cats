e164b36436fe793c83895bf16c7405328f13d9d1
Radoslaw Tomaszewski <radoslaw.tomaszewski@sobre.pl>
2026-02-23 10:38:51 +0100

    Added language switcher flags images

--- src/components/Header/LanguageSwitcher.js
+++ src/components/Header/LanguageSwitcher.js
@@ -1,22 +1,45 @@
 import React from "react"
 import { Link, useI18next } from "gatsby-plugin-react-i18next"
+import polandFlag from "../../images/language-switcher-flags/poland.png"
+import unitedKingdomFlag from "../../images/language-switcher-flags/united-kingdom.png"
+import germanyFlag from "../../images/language-switcher-flags/germany.png"
+
+const languageConfig = {
+  pl: {
+    flag: polandFlag,
+    label: "Polski",
+  },
+  en: {
+    flag: unitedKingdomFlag,
+    label: "English",
+  },
+  de: {
+    flag: germanyFlag,
+    label: "Deutsch",
+  },
+}
 
 const LanguageSwitcher = () => {
-  const { language, languages, originalPath, path } = useI18next()
+  const { languages, language } = useI18next()
   console.log(languages)
 
   return (
     <div style={{ padding: "15px" }}>
-      {languages.map(lng => (
-        <Link
-          key={lng}
-          to={`${lng === "pl" ? "/" : lng === "en" ? "/breeding" : "/zucht"}`}
-          language={lng} // <- bardzo ważne, żeby przekazać język
-          style={{ marginRight: "10px" }}
-        >
-          {lng.toUpperCase()}
-        </Link>
-      ))}
+      {languages.map(lng => {
+        const config = languageConfig[lng]
+        return (
+          <Link
+            key={lng}
+            to={`${lng === "pl" ? "/" : lng === "en" ? "/breeding" : "/zucht"}`}
+            language={lng} // <- bardzo ważne, żeby przekazać język
+            style={{ marginRight: "10px" }}
+          >
+            {language !== lng ? (
+              <img src={config.flag} alt={config.label} width={24} />
+            ) : null}
+          </Link>
+        )
+      })}
     </div>
   )
 }
